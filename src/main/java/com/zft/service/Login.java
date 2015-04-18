cdda4f393586251fb08412a83b730d79b21ae83a
Hossam.AHamid <hossam.ahamid@gmail.com>
2015-04-18 07:10:12 +0200

    Service is up with get notifications

--- src/main/java/com/zft/service/Login.java
+++ src/main/java/com/zft/service/Login.java
@@ -2,6 +2,7 @@ package com.zft.service;
 
 import com.zft.pojo.ServiceType;
 import com.zft.pojo.User;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by Rainfall on 4/18/2015.
  */
 
-@RestController
+@Controller
 @RequestMapping("/web")
 public class Login {
 
@@ -20,10 +21,11 @@ public class Login {
         //TODO: remove this
         User userTemp = new User();
         userTemp.setUserSignature("hos");
+        userTemp.setHashedPhone("12");
         ServiceType.users.add(userTemp);
         for (User user : ServiceType.users){
             if (user.getUserSignature().equals(userSignature))
-                return "success";
+                return "facebook";
         }
         return "error";
     }
@@ -31,12 +33,17 @@ public class Login {
     @RequestMapping(method = RequestMethod.GET, value = "/facebook")
     public String loginFacebook(@RequestParam(value = "user_signature") String userSignature,
                                 @RequestParam(value = "fb") String fbToken){
+
         for (User user : ServiceType.users){
+            System.out.println("param: " + userSignature);
+            System.out.println("users: " + user.getUserSignature());
             if (user.getUserSignature().equals(userSignature)){
                 user.setFbToken(fbToken);
+                System.out.println(fbToken);
                 return "success";
             }
         }
+        System.out.println("Error");
         return "error";
     }
 
--- src/main/java/com/zft/service/Registration.java
+++ src/main/java/com/zft/service/Registration.java
@@ -3,12 +3,22 @@ package com.zft.service;
 import com.zft.pojo.Response;
 import com.zft.pojo.ServiceType;
 import com.zft.pojo.User;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.ServletContext;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by Rainfall on 4/17/2015.
@@ -39,8 +49,31 @@ public class Registration {
                 }
 
                 //Current user to get notifications
-                //call fb.getNotifications(user.getFbToken())
-                return null;
+                HttpClient httpClient = HttpClientBuilder.create().build();
+                HttpGet httpRequest = new HttpGet("https://graph.facebook.com/v2.3/me/notifications?access_token=" + user.getFbToken());
+                HttpResponse httpResponse = null;
+                BufferedReader responseContent = null;
+                StringBuffer result = null;
+                try {
+                    httpResponse = httpClient.execute(httpRequest);
+                    responseContent = new BufferedReader(new InputStreamReader(httpResponse.getEntity().getContent()));
+                    result = new StringBuffer();
+                    String line = "";
+
+                    while ((line = responseContent.readLine()) != null)
+                        result.append(line);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                JSONObject jsonObject = (JSONObject) JSONValue.parse(String.valueOf(result));
+                JSONArray jsonArray = (JSONArray) jsonObject.get("data");
+                String title = "";
+                for (Object object : jsonArray){
+                    title += "-" + ((JSONObject)object).get("title").toString();
+                }
+                return String.valueOf(title);
             }
         }
 
